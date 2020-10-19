2e83ae74a55853031bb79b18d3bba8cc5da7add8
Aleksander <69143632+Aleks277@users.noreply.github.com>
2020-10-19 09:47:43 +0200

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,14 @@
+function myFunction()
+  {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    var element = newFunction();
+
+  }
+  var element = newFunction();
+  function newFunction() {
+    var element = document.getElementById("name");
+    element.classList.toggle("dark-mode2");
+    return element;
+}
